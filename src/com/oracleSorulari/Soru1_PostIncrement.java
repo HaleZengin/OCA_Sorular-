f6e8aa55df633929a7fa629b119abf3318160d64
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-04 16:49:16 +0300

    postIncrement

--- src/com/oracleSorulari/Soru1_PostIncrement.java
+++ src/com/oracleSorulari/Soru1_PostIncrement.java
@@ -4,17 +4,23 @@ public class Soru1_PostIncrement {
 
     public static void main(String[] args) {
 
+        // soru1: num1=9; olarak verildiginde output ne olur?
+
          int num1=9;
          int num2=num1++;
 
          if(num2<10){
-             System.out.println("num2: "+num2+" Hello World");
+             System.out.println(num2+" post increment");
 
          } else{
-             System.out.println("num1: "+num1+" Hello Universel");
+             System.out.println(num1+" pre increment");
          }
 
          // num1 'in yeni degerini gormek icin yazdiralim
         System.out.println("num1: "+num1);
     }
 }
+
+    // int num2=num1++; once atama yapilir sonra num1 degeri 1 artar. buna post increment denir
+    // int num2=++num1; olsaydi once 1 arttiracak sonra atama yapacakti. buna da pre increment denir
+
