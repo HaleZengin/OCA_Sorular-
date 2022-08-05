4f71dd115c656778cc21036ef87e226c1d0141b9
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-05 23:58:19 +0300

    string manipulation

--- src/com/oracleSorulari/Soru4_StringManupulation.java
+++ src/com/oracleSorulari/Soru4_StringManupulation.java
@@ -4,5 +4,17 @@ public class Soru4_StringManupulation {
 
     public static void main(String[] args) {
 
+        String str1= "Java";
+        String str2=new String("java");
+        // 10. satir
+        if(str2.equals(str1.toLowerCase()))
+
+
+        {
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+
+        }
     }
 }
