a953f7ec191bdcfd185419480c4320c4418f685f
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-06 11:45:04 +0300

    string manipulation

--- src/com/oracleSorulari/Soru4_StringManipulation.java
+++ src/com/oracleSorulari/Soru4_StringManipulation.java
@@ -6,9 +6,18 @@ public class Soru4_StringManipulation {
 
         String str1= "Java";
         String str2=new String("java");
-        // 10. satir
-        if(str2.equals(str1.toLowerCase()))
+        // yukaridaki iki String' e hangisi eklenirse sonuc equal olur?
 
+        // str1.toLowerCase();  yaptigimizda str1 kalici olarak degismez.
+
+        //if(str1==str2) bunu kullanmayiz. yerine equals() metodu kullanilir.
+        // equals() sadece String'lerin iceriginin esit olduguna bakar
+        // == ise hem iceriginin hem de referanslarin esit olup olmadigini kontrol eder
+        // String str2=new String("java"); ifadesinde new keyword' u ile bu String yeni bir referans almis olur.
+        // if(str1==str2) yaptigimizda hem referans hem icerigi farklidir. sonuc not equal olur.
+
+
+        if(str2.equals(str1.toLowerCase()))
 
         {
             System.out.println("Equal");
@@ -16,5 +25,7 @@ public class Soru4_StringManipulation {
             System.out.println("Not Equal");
 
         }
+
+        // if(str2.equals(str1.toLowerCase())) yaparak str1'i once harfleri kucuk yapar. sonra da str2 ie sadece icerik olarak esit mi diye bakar.
     }
 }
