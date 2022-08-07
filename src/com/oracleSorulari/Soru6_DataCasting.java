b10a7429887f871faad43264b90bce9fef89f290
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-07 11:57:40 +0300

    data casting

---
+++ src/com/oracleSorulari/Soru6_DataCasting.java
@@ -0,0 +1,33 @@
+package com.oracleSorulari;
+
+public class Soru6_DataCasting {
+    public static void main(String[] args) {
+        /*
+        long x = 10;
+        int y = 2 *  x;
+        yukaridaki soruda nasil degisiklikler yapilarak hata gitmis olur?
+         */
+
+        // data casting; primitive ve sayisal data türünü birbirine cevirmektir.
+        // kucukten buyuge: byte->short->int->long->float->double
+
+        // yukaridaki soruyu bir kac farkli sekilde cozebiliriz.
+    /*
+        // 1
+        long x=10;
+        int y = (int) (2*x);          cast yaptik
+
+        // 2
+        long x=10;
+        long y=2*x;                   int olan data turunu long' a cevirdik
+
+        //3
+        short x=10;                   long olan data turunu short'a cevirdik. yani int' in bir altina indirdik
+        int y=2*x;
+
+     */
+
+
+
+    }
+}
