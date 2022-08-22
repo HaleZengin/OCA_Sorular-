5e12911a169b7b8fc31d4c01ff1505c0f28b918c
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-23 00:07:21 +0300

    method creation

--- src/com/oracleSorulari/Soru11_MethodCeation.java
+++ src/com/oracleSorulari/Soru11_MethodCeation.java
@@ -31,6 +31,15 @@ public class Soru11_MethodCeation {
     20. satir calisir
     value=9 dur
     result variable' nin degerinde bir method call var. biz de bunu takip ederiz ve square icin ustteki methoda gideriz
+    simdi 14. satir calisir
+    square(value) kisminda value degeri 9 du. method'da int x yerine 9 gelir
+    y' nin degeri 9*9 oldu
+    x' in yeni degeri -1 oldu ama kalici degisiklik olmadi
+    return y degeri olarak en sondaki islem gelir 9*9=81
+    y degeri artik value degeridir. 23. satira geri gelir. artik result degeri 81'dir.
+    24. satir calisir. value degeri 9 dur. konsolda 9 yazar.
+
+    cevap B
 
      */
 }
