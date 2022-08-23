83531dc6c1471e694c01d22e827ae050dc497498
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-24 00:10:08 +0300

    while loop

---
+++ src/com/oracleSorulari/Soru14_WhileLoop.java
@@ -0,0 +1,35 @@
+package com.oracleSorulari;
+
+public class Soru14_WhileLoop {
+
+    public static void main(String[] args) {
+
+        int x = 1, y = 15;
+
+
+        //while x < 10
+        y--;
+        x++;
+        System.out.println(x+", "+y);
+
+        /*
+        kod blogu calistirilirsa sonuc ne olur?
+        A. 10, 5
+        B. 10, 6
+        C. 11, 5
+        D. 10. Satirdan dolayi kod calismaz.
+        E. 11. Satirdan dolayi kod calismaz.
+        F. Kod sonsuz bir donguye girer.
+         */
+
+        /*
+        soru 10. satirda hata verir. while' da parantez olmali.
+        cevap D
+
+         */
+
+
+
+
+    }
+}
