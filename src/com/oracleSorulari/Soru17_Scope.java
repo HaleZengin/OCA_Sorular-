efc971c49e5cd756545547b696ebedfa18e8e881
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-08-25 13:39:04 +0300

    do while loop

---
+++ src/com/oracleSorulari/Soru17_Scope.java
@@ -0,0 +1,34 @@
+package com.oracleSorulari;
+
+public class Soru17_Scope {
+
+    public static void main(String[] args) {
+
+        /*
+        bu soru CTE verdigi icin yorum icine alindi. while icindeki y degeri do while loop' unun icinde degil disinda tanimlanmasi gerekirdi
+        yani do' nun body'si icinde tanimlanan y disarida kullanilamaz.
+        buradaki y loop variable' dir. scope' u sadece o loop icinde gecerlidir.
+
+        do {
+            int y = 1;
+            System.out.print(y++ + " ");
+            } while(y <= 10);
+        */
+
+
+        /*
+        kod blogu calisirsa sonuc ne olur?
+
+        A. 1 2 3 4 5 6 7 8 9
+        B. 1 2 3 4 5 6 7 8 9 10
+        C. 1 2 3 4 5 6 7 8 9 10 11
+        D. 13. Satirdan dolayi kod calismaz.
+        E. Kod sonsuz bir donguye girer.
+         */
+
+        /*
+        cevap D
+         */
+
+    }
+}
