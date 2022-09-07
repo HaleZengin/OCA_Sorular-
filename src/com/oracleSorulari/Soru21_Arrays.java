242329c68d0bfcdb5d436f5dc3d247adf7177fcc
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-07 18:04:15 +0300

    array

---
+++ src/com/oracleSorulari/Soru21_Arrays.java
@@ -0,0 +1,26 @@
+package com.oracleSorulari;
+
+public class Soru21_Arrays {
+
+    public static void main(String[] args) {
+
+        char[]c = new char[2];
+        // INSERT CODE HERE
+        int length = c.length;
+        System.out.println(length);
+
+        /*
+        A. int length = c.capacity;
+        B. int length = c.capacity();
+        C. int length = c.length;
+        D. int length = c.length();
+        E. int length = c.size;
+        F. int length = c.size();
+        G. None of the above.
+         */
+
+
+
+    }
+
+}
