2782a28fce5ec9b43e372a157f27081e9afef0a3
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-13 22:35:46 +0300

    for loop

--- src/com/oracleSorulari/Soru29_ForLoop.java
+++ src/com/oracleSorulari/Soru29_ForLoop.java
@@ -20,7 +20,12 @@ public class Soru29_ForLoop {
             E. Kod calistirilirsa exception olusur
          */
 
-
+        /*
+        sayi/=sayi++ kisminda post increment kullanilmis. once atama sonra arttirma yapilacak
+        once sayi yi sayi ya bol 3/3=1 oldu sonra sayinin degerini 1 arttirir. sayi 2 oldu
+        sayi nin degeri 1 olunca sayi>1 nin degeri false oldugu icin loop sadece 1 kez calismis olacak
+        cevap C
+         */
 
     }
 }
