79f383aa7acf6746eba04770b4a07b26682c653a
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-13 22:43:18 +0300

    scope

---
+++ src/com/oracleSorulari/Soru30_Scope.java
@@ -0,0 +1,36 @@
+package com.oracleSorulari;
+
+public class Soru30_Scope {
+
+    private static int $;
+    public static void main(String[] args) {
+
+        String a_b;
+        System.out.print($);
+        // $ variable'si class seviyesinde olusturulmustur. deger atanmasina gerek yoktur eger deger atanmazsa java default deger atar
+        //System.out.print(a_b);  bu satir calismaz
+        // a_b local variable'dir. deger atanmayabilir ama deger atanmadan kullanilamaz. daha sonra deger atanmali
+
+
+
+
+        /*
+        A. 3.satirdan dolayi kod calismaz
+        B. 4.satirdan dolayi kod calismaz
+        C. 7.satirdan dolayi kod calismaz
+        D. 8.satirdan dolayi kod calismaz
+        E. 9.satirdan dolayi kod calismaz
+        F. 0null
+        G. nullnull
+        */
+
+        /*
+        cevap E
+
+         */
+
+        /*
+        class adi variable adi _ $ veya bir harf ile baslayabilir
+         */
+    }
+}
