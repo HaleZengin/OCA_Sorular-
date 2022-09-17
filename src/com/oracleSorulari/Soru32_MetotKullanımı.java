c40cf9e7730a784f243f752c4b8970ff9efaa021
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-17 21:48:59 +0300

    metot kullanımı

--- src/com/oracleSorulari/Soru32_MetotKullanımı.java
+++ src/com/oracleSorulari/Soru32_MetotKullanımı.java
@@ -2,6 +2,24 @@ package com.oracleSorulari;
 
 public class Soru32_MetotKullanımı {
 
-
+    public static void addToInt(int x, int amountToAdd) {
+        x = x + amountToAdd;
+    }
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        Soru32_MetotKullanımı.addToInt(a, b);
+        System.out.println(a);
+    }
 
 }
+
+
+     /*
+        A. 10
+        B. 15
+        C. 25
+        D. 5.satirdan dolayi kod calismaz
+        E. 10.satirdan dolayi kod calismaz
+        F. Yukaridakilerden hicbiri.
+      */
