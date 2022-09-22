3f450ae92be08be86438a502d73798d88d1604fd
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-22 17:54:38 +0300

    metot kullanımı

--- src/com/oracleSorulari/Soru32_MetotKullanımı.java
+++ src/com/oracleSorulari/Soru32_MetotKullanımı.java
@@ -5,6 +5,7 @@ public class Soru32_MetotKullanımı {
     public static void addToInt(int x, int amountToAdd) {
         x = x + amountToAdd;
     }
+
     public static void main(String[] args) {
         int a = 15;
         int b = 10;
