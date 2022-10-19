dfd66a0d77d372a54fba933e17e18d8541beaaae
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-10-19 20:18:23 +0300

    constructor

--- src/com/oracleSorulari/Soru36_Constructor02.java
+++ src/com/oracleSorulari/Soru36_Constructor02.java
@@ -3,5 +3,23 @@ package com.oracleSorulari;
 public class Soru36_Constructor02 {
 
 
-
+    int num;
+    public Soru36_Constructor02(int num) {
+        // INSERT CODE HERE
+        this.num = num;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Soru36_Constructor02(50).num);
+    }
 }
+
+        /*
+        asagidaki satirlardan hangisi 8. satira yazilarak consructor tamamlanirsa
+        kod calistiginda 50 ciktisini verir?
+        A. num = num;
+        B. num = this.num;
+        C. this.num = num;
+        D. Yukaridakilerden hicbiri
+        */
+
+
