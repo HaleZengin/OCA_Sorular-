2a82706f75cc962451d24eec2412bdd83982dc3f
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-12-22 16:55:43 +0300

    varargs

--- src/com/oracleSorulari/Soru43_Varargs.java
+++ src/com/oracleSorulari/Soru43_Varargs.java
@@ -10,17 +10,17 @@ public class Soru43_Varargs {
 
         System.out.println(d.length);
     } /*
-    void m1(String... e, int f){
+    void m1(String... e, int f){                     burada once varargs yazilmis. sonda olmali
         System.out.println(e.length);
     }
      */
     public static void main(String[] args) {
 
         Soru43_Varargs vrg=new Soru43_Varargs();
-        vrg.m1(3,4,5,6,7);
+        vrg.m1(3,4,5,6,7);                      // 1. metot calisr. 4 yazdirir
         //vrg.m1(67,"Hello","Hi");
-        vrg.m1('c',75,80,90);
-        vrg.m1('d',"Hello");
+        vrg.m1('c',75,80,90);                   // ilk parametre char ama 2. string degil. o yuzden c' yi ascıı degerini alarak 1. metot calisacak. b nin lenght'i 3 yazdirir
+        vrg.m1('d',"Hello");                    // 2. metot calisir. 1 yazdirir
     }
 
 }
@@ -30,4 +30,10 @@ public class Soru43_Varargs {
     consoleda cikan sonuc ne olur?
      */
 
+    /*
+    varargs ile metotlarda istedigimiz kadar parametre gonderebiliriz.
+    bu parametreler array olmus olur. index numaralarini yazarak yazdirilir.
+    cvp: 4, 3, 1 yazdirir
 
+
+     */
