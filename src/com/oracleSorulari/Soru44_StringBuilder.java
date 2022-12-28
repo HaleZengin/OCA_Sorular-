48e7c61c9ad3018a96c45f7cbef09a9e957aa219
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-12-28 13:58:36 +0300

    string builder

--- src/com/oracleSorulari/Soru44_StringBuilder.java
+++ src/com/oracleSorulari/Soru44_StringBuilder.java
@@ -3,16 +3,16 @@ package com.oracleSorulari;
 public class Soru44_StringBuilder {
 
 
-    public static StringBuilder work(StringBuilder a,
+    public static StringBuilder work(StringBuilder a,                   // metot olusturulmus
                                      StringBuilder b) {
         a = new StringBuilder("a");
         b.append("b");
         return a;
     }
     public static void main(String[] args) {
-        StringBuilder s1 = new StringBuilder("s1");
-        StringBuilder s2 = new StringBuilder("s2");
-        StringBuilder s3 = work(s1, s2);
+        StringBuilder s1 = new StringBuilder("s1");                     // s1 objesi oluturulmus
+        StringBuilder s2 = new StringBuilder("s2");                     // s2 objesi oluturulmus
+        StringBuilder s3 = work(s1, s2);                                // s3 objesi oluturulmus
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
         System.out.println("s3 = " + s3);
